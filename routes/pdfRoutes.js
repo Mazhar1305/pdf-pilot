64656a15715e5e703a6f3ef01b233ac09bb4791d
EhanShareef <ehanshareef342@gmail.com>
2026-06-19 08:08:09 +0530

    Implement PDF extract pages API

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { uploadPdf } from "../middleware/uploadMiddleware.js";
-import { splitPdf } from "../controllers/pdfController.js";
+import {
+  splitPdf,
+  extractPages
+} from "../controllers/pdfController.js";
 
 const router = express.Router();
 
 router.post("/split", uploadPdf, splitPdf);
+
+router.post(
+  "/extract-pages",
+  uploadPdf,
+  extractPages
+);
 
 export default router;
