67b6228c1ab9d645b31973550f17330215c58806
nihan-98716 <nihananoop2020@gmail.com>
2026-06-27 14:37:18 +0530

    feat: implement pdf unlock api

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ import pdfEditRoutes from "./routes/pdfEditRoutes.js";
 import pdfRedactRoutes from "./routes/pdfRedactRoutes.js";
 import pdfCompareRoutes from "./routes/pdfCompareRoutes.js";
 import protectRoutes from "./routes/protectRoutes.js";
+import unlockRoutes from "./routes/unlockRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
@@ -44,6 +45,7 @@ app.use("/api/pdf", organizeRoutes);
 app.use("/api/pdf", ocrRoutes);
 app.use("/api/pdf", removePagesRoutes);
 app.use("/api/pdf", protectRoutes);
+app.use("/api/pdf", unlockRoutes);
 app.use("/api", wordToPdfRoutes);
 app.use("/api", excelToPdfRoutes);
 app.use("/api", htmlToPdfRoutes);
